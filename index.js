fbe9ed90e466d675191e8dec24758d5ef1e1a9ac
aqsaghazal <ghazalaqsa344@gmail.com>
2024-04-03 02:14:12 +0500

    this is a todos list

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+{
+    let addTask = await inquirer.prompt([
+        { name: "todo",
+            type: "input",
+            message: "what do you want to add in your todos list?",
+        },
+        {
+            name: "addMore",
+            type: "conforim",
+            message: "do you want to add more?",
+            default: "yes/no",
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
